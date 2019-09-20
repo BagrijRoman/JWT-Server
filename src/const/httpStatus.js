e361d10bd246e402336ccd8dbd6a2c97968c54c6
BagrijRoman <bagrijroman@gmail.com>
2019-09-20 14:26:38 +0300

    remove no content status

--- src/const/httpStatus.js
+++ src/const/httpStatus.js
@@ -1,6 +1,5 @@
 const OK = 200;
 const CREATED = 201;
-const NO_CONTENT = 204;
 const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
 const FORBIDDEN = 403;
@@ -11,7 +10,6 @@ const BAD_GATEWAY = 502;
 const httpStatus = {
   OK,
   CREATED,
-  NO_CONTENT,
   BAD_REQUEST,
   UNAUTHORIZED,
   FORBIDDEN,
