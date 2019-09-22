3c30be255d810e954dac901d41f8ddd377b47aa8
BagrijRoman <bagrijroman@gmail.com>
2019-09-23 01:28:30 +0300

    update validation error handling

---
+++ src/const/errors.js
@@ -0,0 +1,7 @@
+const VALIDATION_ERROR = 'validationError';
+const VALID_EMAIL_SHOULD_BE_PROVIDED = 'validEmailShouldBeProvided';
+
+export default {
+  VALIDATION_ERROR,
+  VALID_EMAIL_SHOULD_BE_PROVIDED,
+};
--- src/controllers/auth/validators/signIn.js
+++ src/controllers/auth/validators/signIn.js
@@ -1,8 +1,10 @@
 import { celebrate, Joi } from 'celebrate';
 
+import { errors } from '../../../const/';
+
 const signInValidator = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
+    email: Joi.string().email().required().error(() => errors.VALID_EMAIL_SHOULD_BE_PROVIDED),
     password: Joi.string().min(6).max(20).required(),
   }),
 });
