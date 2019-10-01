8eb0125f6a8b1d9125204ca365aee9b13f3ffb58
BagrijRoman <bagrijroman@gmail.com>
2019-10-01 17:03:08 +0300

    add reset password routes base

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -45,6 +45,20 @@ const setupAuthRoutes = (router) => {
       changePassword
     );
 
+  router.route('/reset-password-request')
+    .post(
+      // validate email
+      // check existing reset password tokens
+      // generate reset password token
+    );
+
+  router.route('/reset-password')
+    .post(
+      // validate provided passwords data
+      // check provided reset password token
+      // change password and mark token as used
+    );
+
   return router;
 };
 
