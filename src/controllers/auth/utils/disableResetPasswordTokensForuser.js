2e515edcb1dc9dc44432889e1843a7c28c74d3d9
BagrijRoman <bagrijroman@gmail.com>
2019-10-03 20:45:55 +0300

    add disableAllUserRequests method for resetPasswordRequestsSchema

--- src/controllers/auth/utils/disableResetPasswordTokensForuser.js
+++ src/controllers/auth/utils/disableResetPasswordTokensForuser.js
@@ -1,10 +1,17 @@
+import { logger } from '../../../utils';
+import { ResetPasswordRequests } from '../../../models';
+
 const disableResetPasswordTokensForUser = async (userId) => {
   try {
+
+
+
 
     // todo here disable all existing reset password tokens for user
 
   } catch (err) {
-
+    logger.error(`disableResetPasswordTokensForUser:: userId ${userId}  error details: ${err.toString()}`);
+    throw err;
   }
 };
 
--- src/models/resetPasswordRequests/model.js
+++ src/models/resetPasswordRequests/model.js
@@ -2,4 +2,14 @@ import { model } from 'mongoose';
 
 import resetPasswordRequestsSchema from './schema';
 
+resetPasswordRequestsSchema.statics.disableAllUserRequests = async function (userId) {
+  const { nModified } = await this.updateMany(
+    { userId, used: false, disabled: false },
+    { disabled: true }
+  );
+
+  return nModified;
+};
+
+
 export default model('resetPasswordRequests', resetPasswordRequestsSchema);
